16476f6462544ca1ab3c23d105e5a061a77370c2
ziruihao <45774151+ziruihao@users.noreply.github.com>
2019-05-03 19:50:35 -0400

    switched redirect_uri to heroku

--- app.js
+++ app.js
@@ -7,41 +7,41 @@
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
 
-require('dotenv').config();
-dotenv.config({ silent: true });
+// require('dotenv').config();
+// dotenv.config({ silent: true });
 
-var express = require('express'); // Express web server framework
-var request = require('request'); // "Request" library
-var cors = require('cors');
-var querystring = require('querystring');
-var cookieParser = require('cookie-parser');
+// var express = require('express'); // Express web server framework
+// var request = require('request'); // "Request" library
+// var cors = require('cors');
+// var querystring = require('querystring');
+// var cookieParser = require('cookie-parser');
 
-var client_id = process.env.CLIENT_ID; // Your client id
-var client_secret = process.env.CLIENT_SECRET; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+// var client_id = process.env.CLIENT_ID; // Your client id
+// var client_secret = process.env.CLIENT_SECRET; // Your secret
+// var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
-/**
- * Generates a random string containing numbers and letters
- * @param  {number} length The length of the string
- * @return {string} The generated string
- */
-var generateRandomString = function(length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+// /**
+//  * Generates a random string containing numbers and letters
+//  * @param  {number} length The length of the string
+//  * @return {string} The generated string
+//  */
+// var generateRandomString = function(length) {
+//   var text = '';
+//   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
+//   for (var i = 0; i < length; i++) {
+//     text += possible.charAt(Math.floor(Math.random() * possible.length));
+//   }
+//   return text;
+// };
 
-var stateKey = 'spotify_auth_state';
+// var stateKey = 'spotify_auth_state';
 
-var app = express();
+// var app = express();
 
 app.use(express.static(__dirname + '/public'))
    .use(cors())
-   .use(cookieParser());
+  //  .use(cookieParser());
 
 app.get('/login', function(req, res) {
 
