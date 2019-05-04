62f5afd70722b15875fd3643f95ae9b361ddff52
ziruihao <45774151+ziruihao@users.noreply.github.com>
2019-05-04 15:02:55 -0400

    added cors in res.header

--- src/server.js
+++ src/server.js
@@ -43,15 +43,6 @@ app.use(cookieParser());
 // enable/disable http request logging
 app.use(morgan('dev'));
 
-// enable only if you want templating
-app.set('view engine', 'ejs');
-
-// enable only if you want static assets from folder static
-app.use(express.static('static'));
-
-// this just allows us to render ejs from the ../app/views directory
-app.set('views', path.join(__dirname, '../src/views'));
-
 // enable json message body for posting data to API
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -68,13 +59,15 @@ app.get('/login', (req, res) => {
   console.log(state);
   res.cookie(stateKey, state);
 
-  console.log(querystring.stringify({
-    response_type: 'code',
-    client_id: client_id,
-    scope: scope,
-    redirect_uri: redirect_uri,
-    state: state
-  }));
+  // console.log(querystring.stringify({
+  //   response_type: 'code',
+  //   client_id: client_id,
+  //   scope: scope,
+  //   redirect_uri: redirect_uri,
+  //   state: state
+  // }));
+
+  res.header("Access-Control-Allow-Origin", "*");
 
   console.log(`client_id: ${client_id}`);
   var scope = 'user-read-private user-read-email streaming user-top-read';
