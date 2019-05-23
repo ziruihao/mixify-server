e6a93a261811fe6b3f939b29c73cd4096a4c177e
ziruihao <45774151+ziruihao@users.noreply.github.com>
2019-05-23 00:09:46 -0400

    removed cors

--- src/server.js
+++ src/server.js
@@ -54,12 +54,12 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// adding cors
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// // adding cors
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.get('/', (req, res) => {
   res.send('hi');
